3bb2844b17852aaba691a401cb0aa2d2a3fcf52b
Hossain <arafat.kd99@gmail.com>
2023-10-17 16:46:44 +0100

    Validate props for Button component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+import Calculator from './components/Calculator';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+      <Calculator />
     </div>
   );
 }
