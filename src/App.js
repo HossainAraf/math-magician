1c4b99bd0895910a786db197a09b866516eab97e
Hossain <arafat.kd99@gmail.com>
2023-10-20 17:12:07 +0100

    Add quotes component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/Calculator/Calculator';
+import DisplayQuotes from './components/Quotes/DisplayQuotes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <DisplayQuotes />
     </div>
   );
 }
