065bc7d9b20a561d44d653b92d2b888dc6e97d32
Hossain <arafat.kd99@gmail.com>
2023-10-27 21:59:53 +0100

    Update Home page style

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import './Home.module.css';
 
 const Home = () => (
   <main>
-    <h3 style={{
+    <h2 style={{
       padding: '20px 0',
       lineHeight: '1.9em',
-      color: '#fff',
+      color: '#ecc670',
       textAlign: 'center',
-      fontSize: '16px',
+      fontSize: '2rem',
     }}
     >
       Welcome to our Page!
-    </h3>
+    </h2>
     <p>
       Discover a world of numbers and inspiration at Math Magicians.
       Our website offers you two fantastic tools: a feature-rich calculator
       and a daily dose of humor with our Quote of the Day.
     </p>
-    <h2>Calculator:</h2>
+    <h3>Calculator:</h3>
     <p>
       Whether you&apos;re tackling complex math problems or just need a simple calculation,
       our calculator has you covered. It can handle addition, subtraction,
       multiplication, division, percentages, and more. Crunch numbers effortlessly
       with Math Magicians.
     </p>
-    <h2>Quote of the Day:</h2>
+    <h3>Quote of the Day:</h3>
     <p>
       Start your day with a smile by checking out our Quote of the Day.
       We bring you a fresh and funny quote every day to brighten your mood and
